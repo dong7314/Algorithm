b50894752d92b7e27e0b1e0b9cfb08cc2c016319
DongYeopLee <103983410+dong7314@users.noreply.github.com>
2022-08-23 14:22:48 +0900

    [Bronze I] Title: 색종이, Time: 224 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2563. 색종이/색종이.py
@@ -0,0 +1,18 @@
+N = int(input())
+arr = [[0] * 101 for _ in range(101)]
+
+
+for k in range(N):
+    a, b = map(int, input().split())
+
+    for i in range(a, a + 10):
+        for j in range(b, b + 10):
+            arr[i][j] = 1
+
+    result = 0
+    for y in range(101):
+        for x in range(101):
+            if arr[y][x] == 1:
+                result += 1
+
+print(result)
