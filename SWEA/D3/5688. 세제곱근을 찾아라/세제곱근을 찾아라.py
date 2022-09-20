a499184a0e53f36335ddc1ae2828991d1e8f3cf1
DongYeopLee <103983410+dong7314@users.noreply.github.com>
2022-09-20 09:33:30 +0900

    [D3] Title: 세제곱근을 찾아라, Time: 177 ms, Memory: 61,480 KB -BaekjoonHub

--- SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
+++ SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
@@ -1,14 +1,24 @@
-def checking(n):
-    for i in range(n + 1):
-        num = i ** 3
-        if num == n:
-            return i
-        if num > n:
-            return -1
+'''
+3
+27
+7777
+64
+'''
+def binary_search():
+    low = 1
+    high = N
+    while low <= high:
+        mid = (low + high) // 2
+        key = mid ** 3
+        if key == N:
+            return mid
+        elif key > N:
+            high = mid - 1
+        else:
+            low = mid + 1
     return -1
 
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
-
-    print(f'#{tc} {checking(N)}')
+    print(f'#{tc} {binary_search()}')
